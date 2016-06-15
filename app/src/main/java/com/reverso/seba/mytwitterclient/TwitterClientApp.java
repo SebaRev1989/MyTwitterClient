71368139b0ed8305cada62d8e040b508f8e05689
Sebastián Reverso <sheba1989@gmail.com>
2016-06-15 08:19:42 -0300

    Configuracion de Gradle, Twitter y Fabric

---
+++ app/src/main/java/com/reverso/seba/mytwitterclient/TwitterClientApp.java
@@ -0,0 +1,24 @@
+package com.reverso.seba.mytwitterclient;
+
+import android.app.Application;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by seba on 14/06/16.
+ */
+public class TwitterClientApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initFabric();
+    }
+
+    private void initFabric() {
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_KEY, BuildConfig.TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+    }
+}
