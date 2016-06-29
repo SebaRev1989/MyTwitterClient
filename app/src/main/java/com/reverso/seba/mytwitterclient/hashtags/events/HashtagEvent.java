2afe1576f6624b3bdeb15fe289f7cdfcce54d45a
Sebastián Reverso <sheba1989@gmail.com>
2016-06-29 09:55:34 -0300

    Eventos para hashtags

--- app/src/main/java/com/reverso/seba/mytwitterclient/hashtags/events/HashtagEvent.java
+++ app/src/main/java/com/reverso/seba/mytwitterclient/hashtags/events/HashtagEvent.java
@@ -1,8 +1,29 @@
 package com.reverso.seba.mytwitterclient.hashtags.events;
 
+import com.reverso.seba.mytwitterclient.hashtags.entities.Hashtag;
+
+import java.util.List;
+
 /**
  * Created by seba on 29/06/16.
  */
 public class HashtagEvent {
+    private String error;
+    private List<Hashtag> hashtags;
 
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public List<Hashtag> getHashtags() {
+        return hashtags;
+    }
+
+    public void setHashtags(List<Hashtag> hashtags) {
+        this.hashtags = hashtags;
+    }
 }
