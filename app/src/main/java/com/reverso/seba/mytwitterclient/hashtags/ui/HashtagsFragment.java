1427d6ae4c47fc05954dbc9739824ffc907e494c
Sebastián Reverso <sheba1989@gmail.com>
2016-06-29 09:51:15 -0300

    Fragmento para hashtags

--- app/src/main/java/com/reverso/seba/mytwitterclient/hashtags/ui/HashtagsFragment.java
+++ app/src/main/java/com/reverso/seba/mytwitterclient/hashtags/ui/HashtagsFragment.java
@@ -1,32 +1,113 @@
 package com.reverso.seba.mytwitterclient.hashtags.ui;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
 import com.reverso.seba.mytwitterclient.R;
+import com.reverso.seba.mytwitterclient.hashtags.HashtagsPresenter;
+import com.reverso.seba.mytwitterclient.hashtags.adapters.HashtagsAdapter;
+import com.reverso.seba.mytwitterclient.hashtags.entities.Hashtag;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HashtagsFragment extends Fragment {
+public class HashtagsFragment extends Fragment implements HashtagsView, OnItemClickListener {
 
+
+    @Bind(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+    @Bind(R.id.container)
+    FrameLayout container;
+
+    HashtagsPresenter presenter;
+    HashtagsAdapter adapter;
 
     public HashtagsFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        presenter.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        presenter.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void showList() {
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideList() {
+        recyclerView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onHashtagsError(String error) {
+        Snackbar.make(container, error, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void setHashtags(List<Hashtag> items) {
+        adapter.setItems(items);
+    }
+
+    @Override
+    public void onItemClick(Hashtag tweet) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(tweet.getTweetUrl()));
+        startActivity(intent);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
